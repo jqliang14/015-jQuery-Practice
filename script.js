6b24b1a572dfc70d881cbe23091bdccdf0314bc3
Jian Q Liang <30308734+jqliang14@users.noreply.github.com>
2021-11-03 17:55:22 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+//Task 0
+$(".example0-div").hide();
+//Task 1
+$(".button1").click(function(){
+    $(".example1-div").hide();
+});
+
+//Task 2
+$(".button2").click(function() {
+    $(".example2-div").append("blah blah blah");
+});
+
+//Task 3"
+$(".button3").click(function() {
+	$(".example3-div").text("Welcome to the Code Nation Fellowship");
+});
+
+//Task 4
+$(".button4").click(function() {
+    $(".example4-div").css("background-color", "blue");
+});
